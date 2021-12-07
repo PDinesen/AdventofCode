27d4cf02f05633c2538e4812e4c8f215705faa30
PAD Work <pad@birke-gym.dk>
2021-12-07 10:00:40 +0100

    From Work PC: Update day 7 with functions

--- 2021work/Day7.py
+++ 2021work/Day7.py
@@ -1,6 +1,4 @@
 ind = [list(map(int, line.split(','))) for line in open('input7.txt')][0]
 
-test = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
-
 print(min(sum(abs(i-el) for el in ind) for i in range(max(ind))))
 print(min(sum(int(((abs(i-el)+1)*abs(i-el))/2) for el in ind) for i in range(max(ind))))
