c28ded20029b2305a521c85ed432930ea50c9c2a
PAD Work <pad@birke-gym.dk>
2021-12-07 09:57:10 +0100

    From Work PC: Update day 6 with functions

--- 2021work/Day6.py
+++ 2021work/Day6.py
@@ -1,24 +1,30 @@
-ind = [list(map(int,line.split(','))) for line in open('input6.txt')][0]
-test=[3,4,3,1,2]
+ind = [list(map(int, line.split(','))) for line in open('input6.txt')][0]
 
-dict_fish = {}
-for i in range(9):
-    dict_fish[i] = sum(j == i for j in ind)
 
-def one_day2(dictonary):
-    zeros = dictonary[0]
-    for key in range(1,9):
-        fishes = dictonary[key]
-        dictonary[key - 1] = fishes
-    dictonary[6] += zeros
-    dictonary[8] = zeros
-    return dictonary
+def initial(input_list):
+    dict_fish = {}
+    for i in range(9):
+        dict_fish[i] = sum(j == i for j in input_list)
+    return dict_fish
 
-def run2(n, dict_fishes):
-    temp = dict_fishes.copy()
+
+def one_day2(dictionary):
+    zeros = dictionary[0]
+    for key in range(1, 9):
+        fish = dictionary[key]
+        dictionary[key - 1] = fish
+    dictionary[6] += zeros
+    dictionary[8] = zeros
+    return dictionary
+
+
+def run(n, input_list):
+    dictionary = initial(input_list)
+    temp = dictionary.copy()
     for _ in range(n):
         temp = one_day2(temp)
     print(sum(i for i in temp.values()))
 
-run2(80,dict_fish)
-run2(256,dict_fish)
+
+run(80, ind)
+run(256, ind)
--- 2021work/utils/constants.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-cached_file_dir: Path = Path(__file__).parent.parent / 'cached_files'
-correct_answers_dir: Path = Path(__file__).parent.parent / 'correct_answers'
-current_year = 2021
