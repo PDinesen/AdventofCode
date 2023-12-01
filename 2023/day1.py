bbd03a8605d4d8fa0cfdd538448e29c66cfe8dc0
PAD Work <pad@birke-gym.dk>
2023-12-01 19:39:09 +0100

    From work PC: 23 day 1 += og modulo

--- 2023/day1.py
+++ 2023/day1.py
@@ -4,16 +4,16 @@ def solve(txt, task2=False):
     res = 0
     test = [str(i) for i in range(10)]
     if task2:
-        test = [' ', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'] + test
+        test += [' ', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     for word in st:
         low = (len(word), -1)
         high = (-1, -1)
         for i, tjek in enumerate(test):
             if tjek in word:
                 if word.index(tjek) < low[0]:
-                    low = (word.index(tjek), int(str(i)[-1]))
+                    low = (word.index(tjek), i % 10)
                 if word.rindex(tjek) > high[0]:
-                    high = (word.rindex(tjek), int(str(i)[-1]))
+                    high = (word.rindex(tjek), i % 10)
 
         res += low[1] * 10 + high[1]
 
